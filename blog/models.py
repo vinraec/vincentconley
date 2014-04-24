95c90534cfbe6bdafb15447d3569f943c523f1c8
Vincent Conley <conleyv@wharton.upenn.edu>
2014-04-23 23:28:17 -0400

    Calling it a night, set up first models for the blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    body = models.TextField()
+    posted = models.DateTimeField(db_index=True, auto_now_add=True)
+    category = models.ManyToManyField('blog.Category')
+
+    def __str__(self):
+        return '%s' % self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self):
+        return '%s' % self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from blog.models import Post, Category
+
+
+def homepage(request):
+    return render_to_response('blog/home.html')
+
 
 # Create your views here.
